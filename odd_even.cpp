fad36bf99a2a9784ec973d72a19dea8396d3e49d
anilkumarpaital1-art <anilkumarpaital1@gmail.com>
2026-05-21 02:00:07 +0530

    Added even odd program

---
+++ odd_even.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    if(n%2==0){
+        cout<<"Print Even";
+    }
+    else{
+        cout<<"Print Odd";
+    }
+    return 0;
+}
