14a49cfa3d1e2ca75283e5893a631aa41ccd9860
anilkumarpaital1-art <anilkumarpaital1@gmail.com>
2026-05-21 01:36:27 +0530

    Updated gitignore

---
+++ simple_interest.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int p,r,t,SI;
+    cout<<"Enter the value of p:";
+    cin>>p;
+    cout<<"Enter the value of r:";
+    cin>>r;
+    cout<<"Enter the value of t:";
+    cin>>t;
+
+    SI=(p*r*t)/100;
+
+    cout<<"The simple interest is:"<<SI;
+
+    return 0;
+
+}
