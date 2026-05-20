b591ac95a82465da8e3984214efc5e2551da0f44
anilkumarpaital1-art <anilkumarpaital1@gmail.com>
2026-05-21 01:02:43 +0530

    Added first C++ DSA program

---
+++ add_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a,b,sum;
+    cout<<"Enter the value of a:";
+    cin>>a;
+    cout<<"Enter the value of b:";
+    cin>>b;
+    sum=a+b;
+    cout<<"The sum of a and b are:"<<sum;
+
+    return 0;
+}
