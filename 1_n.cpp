dd4064d2d241399bc7678b418a886157504a7ded
anilkumarpaital1-art <anilkumarpaital1@gmail.com>
2026-05-21 02:38:57 +0530

    Add

---
+++ 1_n.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Print the numbers: ";
+
+    for(int i = 1; i <= n; i++){
+        cout << i << " ";
+    }
+
+    return 0;
+}
